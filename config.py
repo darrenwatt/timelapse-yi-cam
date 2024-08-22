c0c8206b23e514d53487fcffee2829f648bf625f
Darren Watt <thedarrenwatt@gmail.com>
2024-08-22 23:08:33 +0100

    working

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config(object):
-    WEBCAM_RTSP = os.getenv('WEBCAM_RTSP')
-    WEBCAM_PICS_DIR = os.getenv('WEBCAM_PICS_DIR')
+    WEBCAM_RTSP = os.getenv('WEBCAM_RTSP') # rtsp URL, e.g. 'rtsp://<ip address>/ch0_0.h264'
+    WEBCAM_PICS_DIR = os.getenv('WEBCAM_PICS_DIR') # base url to store snapshots
--- timelapse-get.py
+++ timelapse-get.py
@@ -5,7 +5,6 @@ import shutil
 import time
 from config import Config
 
-
 vcap = cv2.VideoCapture(Config.WEBCAM_RTSP)
 
 topdir = Config.WEBCAM_PICS_DIR
@@ -13,14 +12,14 @@ topdir = Config.WEBCAM_PICS_DIR
 if vcap.isOpened():
     ret, frame = vcap.read()
     if ret:
-       cv2.imwrite('/code/src/static/img/webcam.png', frame)
+       cv2.imwrite('.tmp.png', frame)
        filename = time.strftime("%Y%m%d-%H%M%S")
        dirpath = time.strftime("%Y/%m/%d/")
-       if not os.path.isdir(f'{topdir}{dirpath}'):
-           os.makedirs(f'{topdir}{dirpath}')
-           
-       print(f'{topdir}{dirpath}{filename}.png')
-       shutil.copy2('/code/src/static/img/webcam.png', f'{topdir}{dirpath}{filename}.png')
+       if not os.path.isdir(f'{topdir}/{dirpath}'):
+           os.makedirs(f'{topdir}/{dirpath}')
+
+       print(f'{topdir}/{dirpath}{filename}.png')
+       shutil.move('.tmp.png', f'{topdir}/{dirpath}{filename}.png')
     else:
         print("Failed to Capture Frame")
 else:
